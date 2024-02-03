3945756317ac776d7bc294dd3c38d3a0d5bbc304
dev22 <ysoli36512@gmail.com>
2024-02-03 13:03:40 +0900

    #83-코드 리팩토링 * `SecurityConfig`: 줄바꿈 수정. 간단한 Javadoc 문법 소개 * `UserAccount`: `new` 생성자를 `of()` 메소드로 변경 * `BoardPrincipal`: `of()` 메소드에 클래스명까지 작성

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.UUID;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -62,12 +64,23 @@ public class SecurityConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
     }
 
+    /**
+     * <p>
+     * OAuth 2.0 기술을 이용한 인증 정보를 처리한다.
+     * 카카오 인증 방식을 선택.
+     *
+     * <p>
+     * TODO: 카카오 도메인에 결합되어 있는 코드. 확장을 고려하면 별도 인증 처리 서비스 클래스로 분리하는 것이 좋지만, 현재 다른 OAuth 인증 플랫폼을 사용할 예정이 없어 이렇게 마무리한다.
+     *
+     * @param userAccountService  게시판 서비스의 사용자 계정을 다루는 서비스 로직
+     * @param passwordEncoder 패스워드 암호화 도구
+     * @return {@link OAuth2UserService} OAuth2 인증 사용자 정보를 읽어들이고 처리하는 서비스 인스턴스 반환
+     */
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
             UserAccountService userAccountService,
             PasswordEncoder passwordEncoder
-    ){
-        //부득이 하게 new로 접근하여 사용한다. 한번 사용하는 것이다. 대체로 Ioc 컨테이너에서 생성된 것을 사용한다.
+    ) {
         final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
         return userRequest -> {
@@ -75,9 +88,9 @@ public class SecurityConfig {
 
             KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
             String registrationId = userRequest.getClientRegistration().getRegistrationId();
-            String providerId = kakaoResponse.id().toString();
+            String providerId = String.valueOf(kakaoResponse.id());
             String username = registrationId + "_" + providerId;
-            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy");
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
 
             return userAccountService.searchUser(username)
                     .map(BoardPrincipal::from)
@@ -94,6 +107,7 @@ public class SecurityConfig {
                     );
         };
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
